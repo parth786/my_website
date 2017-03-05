1bf271eae7617fb6a75e36c607a5ec4085b75854
parth786 <parthrocks89@yahoo.in>
2017-03-04 23:39:12 -0500

    commit

--- javascript.js
+++ javascript.js
@@ -3,11 +3,11 @@ $(function(){
     
     $("#cover_jumbotron").hide();
     $("#cover_jumbotron").fadeIn(1500);
-    $("#cover_jumbotron").queue(function(){
-        $("#my_name").addClass("animated flash");
-        $("#my_thaught").addClass("animated shake");
-    
-    });
+//    $("#cover_jumbotron").queue(function(){
+//        $("#my_name").addClass("animated flash");
+//        $("#my_thaught").addClass("animated shake");
+//    
+//    });
     
 //    
 //    $(".project_card").hover(function(){
